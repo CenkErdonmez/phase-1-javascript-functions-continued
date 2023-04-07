71c3b1021ad2945da71ba0df5479e0a8bb18b516
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-04-07 13:44:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return (`This Saturday, I want to ${fun}!`)
+}
+function mondayWork(activity="go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(emphatic){
+    return function(special){
+        return(`You are ${emphatic}${special}${emphatic}!`)
+    }
+}
